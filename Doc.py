42778b23184651e755bc216391349aa70f3330c4
Duanny-Silva <yasmimduanny18@gmail.com>
2022-12-12 17:19:51 -0300

    código do raphael

--- Doc.py
+++ Doc.py
@@ -1,1 +1,12 @@
-print("Hello world")
+class Carro():
+    def __init__(self):
+        self.cor = "laranja"
+        self.marca = "corsa"
+        self.placa = "DT986756JD"
+
+    def mostrar(self):
+        print(f"A cor é: {self.cor}, da marca {self.marca} e placa {self.placa}")
+
+
+Carro2 = Carro()
+Carro2.mostrar()

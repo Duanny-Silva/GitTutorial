78ce07d5dd38ec3ae1311ff3c88f6fa9840c3352
Duanny-Silva <yasmimduanny18@gmail.com>
2022-12-21 09:24:10 -0300

    rafael dnv

--- Doc.py
+++ Doc.py
@@ -1,1 +1,12 @@
-print("Olá puta")
+class Carro():
+    def __init__(self):
+        self.cor = "laranja"
+        self.marca = "corsa"
+        self.placa = "DT986756JD"
+
+    def mostrar(self):
+        print(f"A cor é: {self.cor}, da marca {self.marca} e placa {self.placa}")
+
+
+Carro2 = Carro()
+Carro2.mostrar()
